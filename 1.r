a66572f6d764a0aeacda30726c2692367d660f70
izeh <31554814+izeh@users.noreply.github.com>
2018-04-29 21:30:48 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -1,8 +1,8 @@
-biling.survey = function(N = 40, pBi = .5, ...){
+biling.survey <- function(N = 40, pBi = .5, ...){
   
 Responses = sample(0:1, size = N, prob = c(1 - pBi, pBi), replace = TRUE)  # Generate N responses from parents
                                                                            # (B = 1, M = 0)
-Prop = cumsum(Responses) / 1:N   # Compute running proportion of B as each parent responds
+Prop = cumsum(Responses) / 1:N   # Compute proportion of B as each parent responds
    
 par(las = 1, tck = -.02, font.lab = 2, ...)  
 plot.ts(Prop, type = "o", ylim = 0:1, yaxt = "n", pch = 21, bg = 3, xlab = "Parent ID number", 
