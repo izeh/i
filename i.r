9d422f4cafd1720e074a85c9c4c9c476df31752d
izeh <31554814+izeh@users.noreply.github.com>
2019-03-31 17:15:01 -0500

    Update i.r

--- i.r
+++ i.r
@@ -1,31 +1,5 @@
 
-Break = "\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n"
 
-notice = "   \"bayesL2\", a suite of R functions for Bayesian estimation in L2 research.
-    Copyright (C) 2017-present  Reza Norouzian, rnorouzian@gmail.com
-
-    This program is free software: you can redistribute it under the 
-    terms of the GNU General Public License as published by the Free 
-    Software Foundation, either version 3 of the License, or any later 
-    version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>."
-
-message(Break, notice, Break)
-
-Break = "\n*****************************************************************************\n"
-
-cite = "To cite the package use:\n\nNorouzian, R., de Miranda, M. A., & Plonsky, L. (2018). The Bayesian \nrevolution in second language research: An applied approach. Language Learning, 64, 1032-1075.
-
-\nNorouzian, R., de Miranda, M. A., & Plonsky, L. (2019). A Bayesian approach to measuring evidence \nin L2 research: An empirical investigation. Modern Language Journal, 103, 248-263."
-
-cat(Break, cite, Break)
 
 #==================================================================================================================
 
