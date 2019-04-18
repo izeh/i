94fdffca7269a64c2d1b367eff63ad362789edfa
izeh <31554814+izeh@users.noreply.github.com>
2019-04-18 11:31:40 -0500

    Update i.r

--- i.r
+++ i.r
@@ -8111,7 +8111,34 @@ d.ci.default <- function(d, t = NA, n1, n2 = NA, conf.level = .95, digits = 1e2,
   
 }                               
                    
-                   
+inv <- function (X, tol = sqrt(.Machine$double.eps)) 
+{
+  if (length(dim(X)) > 2L || !(is.numeric(X) || is.complex(X))) 
+    stop("'X' must be a numeric or complex matrix")
+  if (!is.matrix(X)) 
+    X <- as.matrix(X)
+  Xsvd <- svd(X)
+  if (is.complex(X)) 
+    Xsvd$u <- Conj(Xsvd$u)
+  Positive <- Xsvd$d > max(tol * Xsvd$d[1L], 0)
+  if (all(Positive)) 
+    Xsvd$v %*% (1/Xsvd$d * t(Xsvd$u))
+  else if (!any(Positive)) 
+    array(0, dim(X)[2L:1L])
+  else Xsvd$v[, Positive, drop = FALSE] %*% ((1/Xsvd$d[Positive]) * 
+       t(Xsvd$u[, Positive, drop = FALSE]))
+}
+
+#===========================================================================================================================
+                  
+ave.drr <- function(d, r, weight){
+  
+p <- eq(d, weight)
+d <- p[[1]] ; weight <- p[[2]]
+as.numeric(crossprod(d, weight))/ sqrt(r*sum(weight)^2 + (1-r)*sum(weight^2))
+
+}     
+                  
 #===========================================================================================================================
                                         
 # need <- c("rstanarm") #, "pscl", "glmmTMB")  #, "arrangements")
